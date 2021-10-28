7cde4d168a498379238238b98933a08d2eb59a88
AndresTam <19030296@itcelaya.edu.mx>
2021-10-28 13:58:49 -0500

    Tercera Version

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #Importación de las librerias utilizadas
-import random as rnd
-import time
+import random as rnd  #Importa la libreria usada para generar numeros aleatorios
+import time #Importa la libreria necesaria para trabajar el retardo del tiempo
+import os #Importa la libreria necesaria para borrar la impresion en consola
 
 #--------Generación de las funciones--------#
     #---Función para imprimir el llenado de vasos
 def printCups(colorsArray):
+    """
+    Esta función sirve para poder imprimir los vasos dentro de la consola,
+    poniendo cada color dentro de los mismos, guiandose por el arreglo donde
+    se almacenaron previamente.
+    """
     print(f"""
         |        |  |        |  |        |
         |________|  |________|  |________|
@@ -16,6 +22,12 @@ def printCups(colorsArray):
 
     #Función para generar la posoción aleatoriamente
 def randomPositions():
+    """
+    Esta función sirve para generar las posiciones aleatoriamente de cada
+    uno de los colores, al macenandola dentro de un arreglo propio, el cual
+    tiene 2 filas y 3 columnas, esto para simular la posicion que tendrá
+    dentro del vaso y poder almacenar los resultados dentro del arreglo de colores.
+    """
     positions = [
         [0,0,0],
         [0,0,0]
@@ -45,6 +57,11 @@ def randomPositions():
 
     #Función para llenar los vasos con retardo
 def timeFiller(colors, positions):
+    """
+    Esta fucnión sirve para poder imprimir los colores de los vasos
+    con un retardo de un segundo, aunque el tiempo puede ser cambiado
+    a preferencia.
+    """
     for i in[0,1]:
         for x in[0,1,2]:
             if positions[i][x] != 0:
@@ -52,11 +69,29 @@ def timeFiller(colors, positions):
                     colors[i][x] = "Azul"
                 else:
                     colors[i][x] = "Rojo"
+            clearConsole()
             printCups(colors)
             time.sleep(1)
 
+    #Funcion para limpiar la consola
+def clearConsole():
+    """
+    Esta fucnión sirve para limpiar la consola durante la ejecución
+    del programa, manteniendo así una salida en consola más limpia
+    y facil de entender.
+    """
+    command = "clear"
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
     #Función para iniciar el llenado de los vasos
 def fillCups():
+    """
+    Esta funcion inicia el proceso de llenado de los vasos,
+    llamando a funciones superiores para poder imprimir y
+    realizar la logica para la generacion de numeros aleatorios.
+    """
     print("El llenado de los vasos comenzará ahora.")
     colors = [
         ["    ","    ","    "],
@@ -64,18 +99,25 @@ def fillCups():
     ]
     printCups(colors)
     timeFiller(colors, randomPositions())
-    exit()
 
     #---Función de inicio del juego
 def startGame():
-    selection = "1"
+    """
+    Esta función sirve para dar inicio al programa de llenado,
+    dentro de esta estan las opciones de inicio y finalización
+    del sistema.
+    """
+    selection = ""
     print("Bienvenido al juego de llenado de vasos. \nPara iniciar introduce el número 1.")
-    while selection != 1:
+    while selection != "1":
         selection = input()
         if selection != "1":
             print("Esa no es una opción en el sistema")
         else:
             fillCups()
+            print("Para salir introduce el número 2.")
+            selection = input()
+            exit()
     return 0;
 
 startGame()
